c2fbf3bd42f348224932439cc5ffdd986f68b753
Viktorguarino <guarino.viktor@hotmail.it>
2017-11-19 19:11:43 +0100

    Add files via upload

--- UF/index.php
+++ UF/index.php
@@ -63,34 +63,18 @@
         </nav>
 		<article>
         	<div class="index">
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner" role="listbox">
-    <div class="carousel-item active">
-      <img class="d-block img-fluid" src="..." alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block img-fluid" src="..." alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block img-fluid" src="..." alt="Third slide">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
+				<div id="slider">
+					<ul id="slideWrap"> 
+						<li style="background-color:red;"></li>
+						<li style="background-color:blue;"></li>
+						<li style="background-color:green;"></li>
+						<li style="background-color:mediumvioletred;"></li>
+						<li style="background-color:greenyellow;"></li>
+				  	</ul>
+				  	<a id="prev" href="#">&#10094;</a>
+				  	<a id="next" href="#">&#10095;</a>
+				</div>
         	</div>
-            
             <script type="text/javascript" src="script.js"></script>
         </article>
         <footer>
